4ace2ecc5d362f93b8e69c9de364f60ea978ad1a
williamang2736 <williamang2736@gmail.com>
2019-04-18 23:03:38 +0800

    added title to dashboard

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Grid from "@material-ui/core/Grid";
+import { Title } from "react-admin";
 
 import Balance from "./Balance";
 import TransactionDoughnuts from "./TransactionDoughnuts";
@@ -7,6 +8,7 @@ import TransactionDoughnuts from "./TransactionDoughnuts";
 export default () => {
   return (
     <div style={{ flexGrow: 1 }}>
+      <Title title="Dashboard" />
       <Grid container spacing={24}>
         <Balance />
         <TransactionDoughnuts />
