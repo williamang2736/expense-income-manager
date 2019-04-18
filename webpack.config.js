75f623502507d6ad3773cf8107d74d3d1a88e856
williamang2736 <williamang2736@gmail.com>
2019-04-18 13:15:06 +0800

    added webpack live reload

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+var LiveReloadPlugin = require("webpack-livereload-plugin");
 
 module.exports = {
   module: {
@@ -11,5 +12,13 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [new LiveReloadPlugin()]
 };
+
+// protocol - (Default: protocol of the page, either http or https) Protocol for livereload <script> src attribute value
+// port - (Default: 35729) The desired port for the livereload server. If you define port 0, an available port will be searched for, starting from 35729.
+// hostname - (Default: hostname of the page, like localhost or 10.0.2.2) The desired hostname for the appended <script> (if present) to point to
+// appendScriptTag - (Default: false) Append livereload <script> automatically to <head>.
+// ignore - (Default: null) RegExp of files to ignore. Null value means ignore nothing.
+// delay - (Default: 0) amount of milliseconds by which to delay the live reload (in case build takes longer)
