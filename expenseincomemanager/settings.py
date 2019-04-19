719b1b954e6351202b335bd29eb5b98a0c3c815f
williamang2736 <williamang2736@gmail.com>
2019-04-19 12:53:56 +0800

    updated debug value in settings.py

--- expenseincomemanager/settings.py
+++ expenseincomemanager/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("EXPENSE_INCOME_APP_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("EXPENSE_INCOME_APP_DEBUG") == "TRUE"
 ALLOWED_HOSTS = ["https://expenseincomemanager.herokuapp.com"]
 
 
